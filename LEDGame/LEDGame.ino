6568275f8f2ad98f47f24afed31f4b232e9fe6d4
Lanmone36 <nazarlan2008@gmail.com>
2024-05-23 13:08:03 +0500

    Fix merge conflict

--- LEDGame/LEDGame.ino
+++ LEDGame/LEDGame.ino
@@ -1,7 +1,5 @@
 #include "./libraries/ButtonManager/ButtonManager.cpp"
 
-#define LED_BTN_COUNT 3
-
 #define LED_BTN_COUNT 3 //Количество светодиодов
 #define MAX_LEVEL 10001//Максимальное количество уровней
 
@@ -22,18 +20,8 @@
 #define LCD_ROWS 2
 #define LCD_ADDR 0x27
 
-<<<<<<< HEAD
 byte* btn_init_list = new byte[LED_BTN_COUNT]{RED_BUTTON, YELLOW_BUTTON, GREEN_BUTTON}; //Костыль, но мне нравится
 ButtonManager btns(btn_init_list, LED_BTN_COUNT);
-
-size_t b;
-=======
-const byte leds[LED_BTN_COUNT] = { RED_LED, YELLOW_LED, GREEN_LED };
-
-Button btns[LED_BTN_COUNT] = { RED_BUTTON, YELLOW_BUTTON, GREEN_BUTTON };
-
-LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS); //LCD дисплей
->>>>>>> b5cb184ce936b21dd66100077b3ea53c00840430
 
 void setup()
 {
